bcb523a4f6197d7ee194c393bdcc6a6b2bcde8ab
Lerry Julian Joostensz <echo.lerryjoostensz@gmail.com>
2022-01-10 02:02:49 +0800

    Tipe Data String

---
+++ 04-TipeDataString.php
@@ -0,0 +1,8 @@
+<?php
+//Single Quete
+echo 'Nama : ';
+echo 'Lerry Julian Joostensz';
+//Dalam Single Quete tidak bisa menambahkan enter atau tab
+//Double Quete
+echo "Nama : ";
+echo "Lerry\t Julian\t Joostensz\n";
