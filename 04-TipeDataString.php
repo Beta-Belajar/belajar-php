ada8125a58eedb6ad092dfb297ae450a076ac17c
Lerry Julian Joostensz <echo.lerryjoostensz@gmail.com>
2022-01-10 02:08:03 +0800

    Heredoc

--- 04-TipeDataString.php
+++ 04-TipeDataString.php
@@ -1,8 +1,16 @@
 <?php
-//Single Quete
+//Single Quote
 echo 'Nama : ';
 echo 'Lerry Julian Joostensz';
-//Dalam Single Quete tidak bisa menambahkan enter atau tab
-//Double Quete
+//Dalam Single Quote tidak bisa menambahkan enter atau tab
+//Double Quote
 echo "Nama : ";
 echo "Lerry\t Julian\t Joostensz\n";
+//Multiline String  bisa menggunakan heredoc dan nowdoc(seperti single qoute)
+//Heredoc -> menggunakan double quote -> tanpa menggunakan \t atau \n dalam pengetikan
+echo <<<TAGNAME
+Selamat belajar PHP
+sekaran, kita belajar tipe data string
+ini adalah :    Cara ke-3 membuat string
+bisa menggunakan heredoc
+TAGNAME;
