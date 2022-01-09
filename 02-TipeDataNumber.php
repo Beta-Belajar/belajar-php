5afdd2f5b9a1ab401de2c72faad0957fb60f7fd9
Lerry Julian Joostensz <echo.lerryjoostensz@gmail.com>
2022-01-09 23:27:18 +0800

    TipeDataNumber -> Floating Point

--- 02-TipeDataNumber.php
+++ 02-TipeDataNumber.php
@@ -13,3 +13,15 @@ var_dump(0b1111);
 
 echo "Underscrore in number : ";
 var_dump(1_234);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point dengan E notation : (1.7 x 1000) : ";
+var_dump(1.7e3);
+
+echo "Floating Point dengan E notation minus : (1.7 x 0.001) : ";
+var_dump(1.7e-3);
+
+echo "Underscore di Floating Point : ";
+var_dump(1.2_3_4);
