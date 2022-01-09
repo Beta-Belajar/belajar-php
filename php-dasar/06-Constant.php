b0e21814c034a436b08ea01350a00b496240331a
Lerry Julian Joostensz <echo.lerryjoostensz@gmail.com>
2022-01-10 02:26:26 +0800

    Constant

---
+++ php-dasar/06-Constant.php
@@ -0,0 +1,9 @@
+<?php
+define("AUTHOR", "ESC17"); //Membuar Constant Menggunaka Upper_Case
+define("APP_VERSION", 100); //Tidak bisa menggunakan nama variable yang sama
+
+
+echo AUTHOR;
+echo "\n";
+echo APP_VERSION;
+echo "\n";
