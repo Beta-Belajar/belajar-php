c1ade6af0516a721543140d18b543b67891b9f09
Lerry Julian Joostensz <echo.lerryjoostensz@gmail.com>
2022-01-09 23:13:04 +0800

    PHP dasar Hello World dan Tipe Data

---
+++ 02-TipeData.php
@@ -0,0 +1,15 @@
+<?php
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(1234);
+
+echo "Underscrore in number : ";
+var_dump(1_234);
