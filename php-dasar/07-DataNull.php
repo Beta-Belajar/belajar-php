0f6bd39b04a8eee2cc80aa5429a9359dd03d83f7
Lerry Julian Joostensz <echo.lerryjoostensz@gmail.com>
2022-01-10 02:34:02 +0800

    Data NUll

---
+++ php-dasar/07-DataNull.php
@@ -0,0 +1,21 @@
+<?php
+$name = "Lerry";
+$name = NULL;
+
+
+$age = null;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo $age;
+echo "\n";
+
+//mengecek apakah data bernilai null atau tidak -> is_null($variable) -> nilai 1 berarti true dan 0 false
+echo "Is Name Null? : ";
+echo is_null($name);
+echo "\n";
+
+var_dump(is_null($name));
