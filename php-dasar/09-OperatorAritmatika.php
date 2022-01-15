595a4b2fb27b99bbadf5aa4edf2ff8b0e748a79d
Lerry Julian Joostensz <echo.lerryjoostensz@gmail.com>
2022-01-15 20:27:07 +0800

    Operator Aritmatika

---
+++ php-dasar/09-OperatorAritmatika.php
@@ -0,0 +1,17 @@
+<?php
+// Operator                    Keterangan
+// +$variabfa                  Positif
+// -$variable                  Negatif
+// $variabel + $variable       Penambahan
+// $variabel - $variable       Pengurangan
+// $variabel * $variable       Perkalian
+// $variabel / $variable       Pembagian
+// $variabel % $variable       Sisa Bagi
+// $variabel ** $variable       Pangkat
+
+
+$result = 10 + 11;
+var_dump($result);
+
+$result = 100 % 3;
+var_dump($result);
