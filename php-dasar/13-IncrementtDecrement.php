7653386ec956347c3353b4b11d4493878eef9411
Lerry Julian Joostensz <echo.lerryjoostensz@gmail.com>
2022-02-07 00:15:12 +0800

    Increment dan Decrement.php

---
+++ php-dasar/13-IncrementtDecrement.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Contoh 
+// $a++            Post Increment          Kembalikan $a lalu naikan 1 angka
+// ++$a            Pre Increment           Naikkan $a satu angka, lalu kembalikan $a
+// $a--            Post Decrement          Kembalikan $a lalu turunkan 1 angka
+ --$a            Pre Decrement           Turunkan $a satu angka, lalu kembalikan $a 
+*/
+$a = 10;
+$b = $a++;
+
+
+var_dump($b);
+?>
