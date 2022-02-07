7831d3fc33b9ab749b0efbdd368193b8120cecd6
Lerry Julian Joostensz <echo.lerryjoostensz@gmail.com>
2022-02-07 16:44:51 +0800

    PHP Function

---
+++ php-dasar/26-Function.php
@@ -0,0 +1,11 @@
+<?php
+// menggunakan kata kunci function diikuti namafunction dan aturannya tidak boleh ada spasi () dan diakhiri blok {}
+function sayHello()
+{
+    echo "Hello Function" . PHP_EOL;
+}
+
+sayHello();
+sayHello();
+
+//php function bisa ditempatkan dimana pun
