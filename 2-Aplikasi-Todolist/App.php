cb5716e24d47e46fe8771961688d7e6647a8235f
Lerry Julian Joostensz <echo.lerryjoostensz@gmail.com>
2022-03-08 21:22:47 +0800

    Membuat View

--- 2-Aplikasi-Todolist/App.php
+++ 2-Aplikasi-Todolist/App.php
@@ -4,6 +4,8 @@ require_once "Model/TodoList.php";
 require_once "BusinessLogic/AddTodoList.php";
 require_once "BusinessLogic/ShowTodoList.php";
 require_once "BusinessLogic/RemoveTodoList";
-
+require_once "Views/ViewAddTodolist.php";
+require_once "Views/ViewRemoveTodolist.php";
+require_once "Views/ViewShowTodolist.php";
 
 echo "Aplikasi Todolist" . PHP_EOL;
