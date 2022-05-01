468f1a9607e0e5bea25273fcf3b8b0b56a4c9d6a
Beta-Belajar <beta.blajar@gmail.com>
2022-05-01 17:12:10 +0800

    Add todolist Logic Test

---
+++ 2-Aplikasi-Todolist/Test/TestaddTodoList.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "../Model/TodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
+
+addTodoList('Lerry');
+addTodoList('Julian');
+addTodoList('Joostensz');
+
+
+var_dump($todoList);
