c1c4edb7563a25fa8e2c64e1748d1ec122469e27
Beta-Belajar <beta.blajar@gmail.com>
2022-05-01 21:06:34 +0800

    Logic View Remove TodoList dengan TestRemoveTodoList

--- 2-Aplikasi-Todolist/Views/ViewRemoveTodolist.php
+++ 2-Aplikasi-Todolist/Views/ViewRemoveTodolist.php
@@ -1,4 +1,21 @@
 <?php
+require_once "../Helper/Input.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
 function viewRemoveTodolist()
 {
+    echo "Menghapus TODO" . PHP_EOL;
+
+    $pilihan = input("Nomor (x untuk batal)");
+
+    $success = removeTodoList($pilihan);
+    if ($pilihan == "x") {
+        echo "Batal menghapus todo" . PHP_EOL;
+    } else {
+
+        if ($success) {
+            echo "Sukses menghapus todo nomor $pilihan" . PHP_EOL;
+        } else {
+            echo "Gagal menghapus todo nomor $pilihan" . PHP_EOL;
+        }
+    }
 }
