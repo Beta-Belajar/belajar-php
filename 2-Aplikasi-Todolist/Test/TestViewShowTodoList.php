a236f2d2d96a9c342301eac8e8c24d33ba1dd302
Beta-Belajar <beta.blajar@gmail.com>
2022-05-01 18:06:22 +0800

    Test VIew TodoList

---
+++ 2-Aplikasi-Todolist/Test/TestViewShowTodoList.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "../Views/ViewShowTodolist.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
+
+addTodoList("Lerry");
+addTodoList("Julian");
+addTodoList("Joostensz");
+
+viewShowTodolist();
--- 2-Aplikasi-Todolist/Views/ViewShowTodolist.php
+++ 2-Aplikasi-Todolist/Views/ViewShowTodolist.php
@@ -1,5 +1,33 @@
 <?php
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../Helper/Input.php";
+require_once "../Views/ViewAddTodolist.php";
+require_once "../Views/ViewRemoveTodolist.php";
+require_once "../Model/TodoList.php";
+
 
 function viewShowTodolist()
 {
+    while (true) {
+        showTodoList();
+
+        echo "Menu" . PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = input("Pilih : ");
+
+        if ($pilihan == "1") {
+            viewAddTodolist();
+        } elseif ($pilihan == "2") {
+            viewRemoveTodolist();
+        } elseif ($pilihan == "x") {
+            break;
+        } else {
+            echo "Pilihan Tidak dimengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
 }
