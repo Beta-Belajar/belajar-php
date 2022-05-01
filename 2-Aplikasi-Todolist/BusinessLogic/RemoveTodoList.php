3461b22616fe6a224a7e70019e9ddd24ed9be204
Beta-Belajar <beta.blajar@gmail.com>
2022-05-01 17:23:22 +0800

    Remove todolist Logic

--- 2-Aplikasi-Todolist/BusinessLogic/RemoveTodoList.php
+++ 2-Aplikasi-Todolist/BusinessLogic/RemoveTodoList.php
@@ -4,6 +4,30 @@
 * Menghapus todolist
 */
 
-function removeTodoList()
+function removeTodoList(int $number): bool
 {
+    global $todoList;
+
+    if ($number > sizeof($todoList)) {
+        return false;
+    }
+    for ($i = $number; $i < sizeof($todoList); $i++) {
+        $todoList[$i] = $todoList[$i + 1];
+    }
+
+    unset($todoList[sizeof($todoList)]);
+    return true;
+
+
+    //1.Lerry
+    //2.Julian
+    //3.Joostensz
+
+    // 1.Lerry
+    // 2. Julian [2+1]
+    // 3. Joostensz[3+1]
+
+
+
+
 }
