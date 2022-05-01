66586528d523f4091911b005755be1d2633a54f4
Beta-Belajar <beta.blajar@gmail.com>
2022-05-01 20:55:30 +0800

    Logic View Add TodoList dengan TestAddTodoList

---
+++ 2-Aplikasi-Todolist/Test/TestViewAddTodoList.php
@@ -0,0 +1,12 @@
+<?php
+require_once "../Views/ViewAddTodolist.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
+
+addTodoList("Lerry");
+
+viewAddTodolist();
+
+
+showTodoList();
